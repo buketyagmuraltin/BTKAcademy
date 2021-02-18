4677cd49d58ebcfd83d140756e50df13c816ee41
buketyagmuraltin <buketyagmuraltin@hotmail.com>
2021-02-18 15:38:00 +0300

    +created: game environment, basic movement system +added: new assets, running animation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D playerRB;
+    Animator playerAnimator;
+    public float moveSpeed = 1f;
+    bool facingRight = true;
+
+    void Start()
+    {
+        playerRB = this.GetComponent<Rigidbody2D>();
+        playerAnimator = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        HorizantalMove();
+        if(playerRB.velocity.x <0 && facingRight)
+        {
+            FlipFace();
+        }
+        else if (playerRB.velocity.x > 0 && !facingRight)
+        {
+            FlipFace();
+        }
+
+    }
+
+    void HorizantalMove()
+    {
+        playerRB.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, playerRB.velocity.y);
+        playerAnimator.SetFloat("playerSpeed", Mathf.Abs(playerRB.velocity.x));
+    }
+
+    void FlipFace()
+    {
+        facingRight = !facingRight;
+        Vector3 tempLocalScale = transform.localScale;
+        tempLocalScale.x *= -1;
+        transform.localScale = tempLocalScale;
+
+    }
+}
